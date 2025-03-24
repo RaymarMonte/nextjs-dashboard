17c78f52322e621bc4c7d689f97a162cee69837b
RaymarMonte <raymarmontecs@gmail.com>
2025-03-24 22:03:16 +0800

    Add initial implementation of invoices page - Add searching functionality to invoices table - Add debounce functionality to invoice search

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,11 +17,9 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
@@ -33,7 +31,6 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     console.log('Fetching latest invoices...');
-    await new Promise((resolve) => setTimeout(resolve, 4000));
 
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -42,7 +39,6 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    console.log('Data fetch completed after 4 seconds.');
 
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
